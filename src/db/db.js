0492e46fb757cddc973f94e1db3a0cf2f72c135e
Alexandre Oliveira <alext.oliver24@gmail.com>
2018-10-24 00:56:13 -0300

    Criando o acesso e tabelas do banco de dados

--- src/db/db.js
+++ src/db/db.js
@@ -1,4 +1,27 @@
-export default callback => {
-	// connect to a database if needed, then pass it to `callback`:
-	callback();
+let mongoose = require('mongoose');
+
+const server = 'ds141043.mlab.com:41043'
+const database = 'zoo'
+
+class Database {
+	constructor() {
+		this._connect()
+	}
+
+	_connect() {
+		mongoose.connect('mongodb://${server}/${database}')
+			.then(() => {
+				console.log('Database connection successful')
+			})
+			.catch(err => {
+				console.error('Database connection error')
+			})
+	}
 }
+
+module.exports = new Database()
+
+// export default callback => {
+// 	// connect to a database if needed, then pass it to `callback`:
+// 	callback();
+// }
---
+++ src/db/models/animals.js
@@ -0,0 +1,31 @@
+let mongoose = require('mongoose');
+
+let AnimalSchema = new mongoose.Schema({
+    localizacao: {
+        type: Schema.ObjectId
+        ref: 'Local',
+        required: true
+    },
+    classe: String,
+    nome_cientifico: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    nome_popular: Array,
+    imagem_url: String,
+    sexo: {
+        type: String,
+        enum: ['macho', 'femea'],
+        required: true
+    },
+    data_nascimento: {
+        type: Date,
+        required: true
+    },
+    descricao: String,
+    nutricao: String,
+    habitat: String
+})
+
+module.exports = mongoose.model('Animal', AnimalSchema)
